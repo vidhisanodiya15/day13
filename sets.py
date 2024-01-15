b7f6c5346a73217d2735eaa2f96cb7b07daefa54
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-01-15 22:43:48 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,13 @@
+# thisset=["cherry","apple","strawberry"]
+# print(thisset)
+
+
+thisset=["cherry","apple","strawberry"]
+for x in thisset:
+    print(x)
+    
+thisset=["cherry","apple","strawberry"]
+
+thisset.add("orange")
+print(thisset)
+            
